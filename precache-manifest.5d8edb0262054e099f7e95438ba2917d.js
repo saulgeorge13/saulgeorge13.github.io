e64e0adc3b2556fa2df8d4f0fbfb20df4f5568be
Saul George <saulsagi@hotmail.com>
2021-05-12 18:00:27 +0400

    Updates

---
+++ precache-manifest.5d8edb0262054e099f7e95438ba2917d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99270058095388e4504eb0a780a03f46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0379f2216070db4701d3",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "291f471688b0108d96fa",
+    "url": "/static/js/2.33ef47b7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.33ef47b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0379f2216070db4701d3",
+    "url": "/static/js/main.08fca49e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "7eae08d3aca196536145f536b518ae0e",
+    "url": "/static/media/ResumeSaul.7eae08d3.pdf"
+  },
+  {
+    "revision": "d8d999890d2bd7352549dc4a7cde971e",
+    "url": "/static/media/profilepic.d8d99989.jpg"
+  }
+]);
